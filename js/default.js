f97aa5cf3eed59d1bbbeeb0152da9d14307835a8
ahnhyunjin1 <a01020813765@gmail.com>
2021-08-02 15:52:05 +0900

    js

--- js/default.js
+++ js/default.js
@@ -4,13 +4,22 @@ $(document).ready(function () {
     spaceBetween: 30,
     loop: true,
     pagination: {
-      el: ".paging",
+      el: ".paging1",
       clickable: true,
     },
     navigation: {
       nextEl: ".slide1 .next",
       prevEl: ".slide1 .prev",
     },
-  })
+  });
 
+
+  var swiper2 = new Swiper(".slide2", {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: ".slide2 .slide2_btn_next",
+      prevEl: ".slide2 .slide2_btn_prev",
+    },
+  });
 });
